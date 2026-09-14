5b55b564e25a647b319ef69f39bb037a0a611151
Thiago Alves <thiago@rapinialves.com>
2026-09-13 22:29:22 -0700

    feat(secrets): onboard slot-01fec2

---
+++ secrets/slot-01fec2/CONTEXT7_API_KEY.sops.sh
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:evIxMNjyMTMoCOVeJaVM9qgVBkZTv7T1q46DEOso56CGjIe3hII7znlJma0wT7BPyk7vFRakqoRFXhXpxyDB+BFkjwJt+A==,iv:D5CFCF4BhIftiOtzP7NOSgwwy7gGTy89MKOV2MLh9kQ=,tag:1oaADdLhpE9lss2SDnwDvQ==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSA0ZXlVOE1XcjkxVHREQWdL\nNlE4QnN3UFIxeDg2azVHYlVpdFdzSnJxU2lvCisxVVM5d3hoc1NzM0FMYXlhaE1T\nQ2lPVC9rTVlTUDNYSWJyU2RJWUl6VFUKLS0tIGQwRlVMaHdHMUhlWmMrTVVhdDVh\nTWNya2JELzJCaHpsa2RjcFJ0N0piRW8Khxt42GnkB6ScOpsnjqhLnyA0c4uOvqAD\nmtQaRO9c++Z5P+X2CKvElH9boQF1VdjLoX37zGPYycHbbOwotF5paw==\n-----END AGE ENCRYPTED FILE-----\n",
+				"recipient": "age1qpgmyz4crx7swuk23ednnhq2ur2q3nfevx0vtcxv6cdmdnu4t5ss30xy3k"
+			}
+		],
+		"lastmodified": "2026-09-14T05:29:22Z",
+		"mac": "ENC[AES256_GCM,data:t9tKEjzxY29WzjCT66HQ+Rr2924ewft27MqcePnr6IgRhIICXbNFSAp880VYnqZrIRSfeUKbqyjzxJndFRHcDs4wj3yKNx+aND0dItqpU8S3Kj5ZytSCbkZFHyrdSsV75LyxXRP/yiJ+48Sk959sadiLG6+rFU7GFb2s6HDEKuY=,iv:Sw5MFgLMwtE03YtLGTSzFy9EiKQhyWkbUz3vWH8uZhA=,tag:Ra5XIf2ZLLgAM4POZ7fO9Q==,type:str]",
+		"version": "3.13.3"
+	}
+}
