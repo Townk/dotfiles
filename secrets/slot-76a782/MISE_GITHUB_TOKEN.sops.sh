8c438f2b415ae1dead8afdee3ac6558c212d5f22
Thiago Alves <thiago@rapinialves.com>
2026-09-13 22:13:58 -0700

    feat(secrets): onboard slot-76a782

---
+++ secrets/slot-76a782/MISE_GITHUB_TOKEN.sops.sh
@@ -0,0 +1,14 @@
+{
+	"data": "ENC[AES256_GCM,data:7xoZ6Lc73V/GwSWECOV6+SfQsuJDdximVDLobtBLB7a6rqoK41Y0Qk1wmplC8ZZ4GGsndcG/P4nkPepWVM02VpvJRTj8dYTX1ZCNt7b7bXP44ItU8ZscM/vhAjy0AB48n1s7ujeKolCxLshJSFoqHOJMV/Hqe2DA+w==,iv:Kx/f0GZdo4vZId+VrRCa/Vizm3nC+5bVyEq7XssQkng=,tag:3UPEk65uWlH4s/EsCA1lng==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSAvdGdXUEU0MUk4ZklSd0R2\nb0xqeUlaaEZyTGdXUUQ5QmxRRWIxMlVwQVRRCjIzUnRMZGFPdU1nbkp3UWlMWVJk\nOXlYWUFOMXAvWUJua25COTJhY1k4WTQKLS0tIHhzbjdtMDRvTGxNem1QNkNEaytH\nMzVMdE9FVFRKV3ZqRUJFNFdaK1pXbnMKxOY4yDC3YVwfvquIIkWSpzKxznKTURq0\naqGuYfjN6ClMaxXc66eASKfXjzLCLOaaiVEMeXZseJ8xAHi9CSs0KA==\n-----END AGE ENCRYPTED FILE-----\n",
+				"recipient": "age1mggpqn50u2uaejwgck6qkfam337da7z6uu4qxse89f92g7jazd6qqu27rz"
+			}
+		],
+		"lastmodified": "2026-09-14T05:13:51Z",
+		"mac": "ENC[AES256_GCM,data:Zq+BFzqYys9RuI9SmqBEVd+pEwBYbafvkLCeOUsrmI70UU2iJfozfM5KSbAhpxTIrNMcr0+xk2KexQJecivNvez3eZAmVIWhbChtC1i1SrVyX+C/H/zxGGgaRl/ykKFliAHh70Ck5Jhq3sr8FXbhwLk/TaGGk3uKaX+eUrKGhSA=,iv:VHnqDNk1d1stH5nEJU3ua8oQD1LayHtvUGQ5AZNE/oU=,tag:UAZ0diAO0wl349ikU2norg==,type:str]",
+		"version": "3.13.3"
+	}
+}
